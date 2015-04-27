213d4403efccf360c531e5c0faa328f25210b2c8
lokesh.pahal <lokesh.pahal@prodigi.io>
2015-04-27 12:24:01 +0530

    changes

--- www/js/app/login.js
+++ www/js/app/login.js
@@ -1,42 +1,52 @@
 var login = {
 	WW: $(window).innerWidth(),
 	WH: $(window).innerHeight(),
-	adjustForm: function(){
-		var formHeight = $('.login-form').height();
-		var topMargin = ((login.WH/2)-(formHeight/2));
-		var inputW = $('.login-form input[type="number"]').outerWidth();
-		
-		$('.login-form').css({marginTop:topMargin});
-		$('.login-button').css({width:inputW+'px'});
-	},
-	adjustForm1: function(){
-		var formHeight = $('.login-form-otp').height();
-		var topMargin = ((login.WH/2)-(formHeight/2));
-		var inputW = $('.login-form-otp input[type="number"]').outerWidth();
-		$('.login-form-otp').css({marginTop:topMargin});
-		$('.login-button').css({width:inputW+'px'});
-	},
+	
 	sendOTP: function(){
 		var mobNumber = $.trim($('.login-form input[type="number"]').val());
 		if(mobNumber!=''){
-			app.navi.pushPage('page1.html')
-			login.adjustForm1();
+			navi.pushPage('page1.html',{ animation : 'slide' })
 		}
 	},
 	back: function(){
-		app.navi.popPage('page.html')
+		navi.popPage('page.html')
+		return false
+	}
+}
+
+$(document).on('pageinit', '#first-page', function() {
+	page.setDeviceBackButtonHandler(function() {
+		//alert("OK: setDeviceBackButtonHandler");
+	});
+});
+app.controller('AppController', function($scope, DeviceBackButtonHandler) {
+	$scope.fire = function() {
+		DeviceBackButtonHandler.fireDeviceBackButtonEvent();
+	};
+	$scope.onDeviceBackButton = function($event) {
+		if ($event.callParentHandler) {
+			navi.popPage('page.html')
+		} else {
+			alert('NG: ng-device-backbutton')
+		}
+	}
+});
+function onDeviceBackButton(event) {
+	if (event.callParentHandler) {
+		//alert('OK: on-device-backbutton');
+	} else {
+		alert('NG: on-device-backbutton');
 	}
 }
 
 $(document).ready(function(){
-	login.adjustForm();
 	$(document).on(appMain.event(),'.login-button',function(){
 		login.sendOTP();
 	});
 	$(document).on(appMain.event(),'.login-button-OTP',function(){
-		login.back();
+		
 	});
 });
 $(window).resize(function(){
-	login.adjustForm();
+	
 });
